15461a7801f90a3c3045e3cc1e038333ee812306
Adarsh gupta <60088161+adarshgupta0601@users.noreply.github.com>
2023-08-09 19:11:10 +0530

    Update Main.java

--- Tiaa-ques1, ques 2/Employee/src/main/java/org/example/Main.java
+++ Tiaa-ques1, ques 2/Employee/src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
         public static void main(String[] args) {
             List<Employee> employees = new ArrayList<>();
